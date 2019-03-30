b63037a505298e7149b10d2f61c473c6a2a344ae
Li Lin <guru.lin@gmail.com>
2019-03-30 22:03:10 +0800

    spring initializer generated project

---
+++ src/test/java/cloudbank/svc/profilesvc/ProfileSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package cloudbank.svc.profilesvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfileSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
