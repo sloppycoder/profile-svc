e1ccbf28f0a50882d764f45ff86c7ea8028cbfc7
Li Lin <guru.lin@gmail.com>
2019-04-06 23:54:55 +0700

    use JSON in response

--- src/main/java/cloudbank/svc/profilesvc/ProfileController.java
+++ src/main/java/cloudbank/svc/profilesvc/ProfileController.java
@@ -4,13 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class ProfileController {
     @Value("${svc.message}")
     String message;
 
     @RequestMapping("/")
-    String showMessage() {
-        return message;
+    Map<String, String> showMessage() {
+        HashMap<String, String> result = new HashMap<>();
+        result.put("message", "hello");
+        result.put("pod", ManagementFactory.getRuntimeMXBean().getName());
+        return result;
     }
 }
